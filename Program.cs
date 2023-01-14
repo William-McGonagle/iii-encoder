9e20dc21c2306a7ddf2b816b8fc21fccc03fd664
William McGonagle <wpmcgonagle@gmail.com>
2023-01-14 16:26:30 -0500

    Made It Runnable

--- Program.cs
+++ Program.cs
@@ -10,26 +10,26 @@ namespace LearnOpenTK
         private static void Main()
         {
 
-            RomanFileHeader header = new RomanFileHeader("_r=4144.422;");
+            // RomanFileHeader header = new RomanFileHeader("_r=4144.422;");
 
-            Console.WriteLine("Is Float Value = " + header.GetFloatValue());
-            Console.WriteLine("Is Channel = " + header.IsChannel());
-            Console.WriteLine(header.ToString());
+            // Console.WriteLine("Is Float Value = " + header.GetFloatValue());
+            // Console.WriteLine("Is Channel = " + header.IsChannel());
+            // Console.WriteLine(header.ToString());
 
-            RomanFile file = RomanFile.ReadFromPath("./res/200.iii");
+            // RomanFile file = RomanFile.ReadFromPath("./res/200.iii");
 
-            // var nativeWindowSettings = new NativeWindowSettings()
-            // {
-            //     Size = new Vector2i(800, 600),
-            //     Title = "LearnOpenTK - Creating a Window",
-            //     // This is needed to run on macos
-            //     Flags = ContextFlags.ForwardCompatible,
-            // };
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(800, 600),
+                Title = "LearnOpenTK - Creating a Window",
+                // This is needed to run on macos
+                Flags = ContextFlags.ForwardCompatible,
+            };
 
-            // using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
-            // {
-            //     window.Run();
-            // }
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
         }
     }
 }
